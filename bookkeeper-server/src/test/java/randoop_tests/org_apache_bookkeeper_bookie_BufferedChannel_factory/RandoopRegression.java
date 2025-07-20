867e9990314587f30dd12271058393896f5adf84
Gaia Meola <gaia.meola@gmail.com>
2025-07-20 15:54:04 +0200

    Generazione automatica di test del costruttore di Buffered Channel con Randoop; provando a renderlo più efficiente ma bisogna migliorare la logica con File channel

---
+++ bookkeeper-server/src/test/java/randoop_tests/org_apache_bookkeeper_bookie_BufferedChannel_factory/RandoopRegression.java
@@ -0,0 +1,10 @@
+package randoop_tests.org_apache_bookkeeper_bookie_BufferedChannel_factory;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RandoopRegression0.class })
+public class RandoopRegression {
+}
+
